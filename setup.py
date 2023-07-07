3eed3d84e3ffd5bf16475209aed34679035d4281
User.py <103832143+SabioPawno@users.noreply.github.com>
2023-07-07 14:55:07 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tempo',
+    version='1.0.0',
+    description='Biblioteca de datas e horas',
+    author='Sabio_',
+    packages=find_packages(),
+    install_requires=['datetime'],
+)
